29b0e23fdfbaae5e3266a8555f095f10cad479b7
Paulo <paulor.s.ext@e-ai.com.br>
2024-05-20 18:23:49 -0300

    feat: :sparkles: migrations

---
+++ migrations/000002-customer.ts
@@ -0,0 +1,29 @@
+import { DATABASE, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } from '@/env';
+import { Client } from 'pg';
+const client = new Client({
+  host: DATABASE_HOST,
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  port: DATABASE_PORT,
+});
+
+export async function up() {
+  await client.connect();
+  await client.query(`CREATE TABLE IF NOT EXISTS customer
+    (
+        id bigserial NOT NULL,
+        name text NOT NULL,
+        cpf varchar(11) NOT NULL,
+        email text NOT NULL,
+        created_at timestamp with time zone NOT NULL DEFAULT now(),
+        PRIMARY KEY (id)
+    );`);
+
+  await client.end();
+}
+
+export async function down() {
+  await client.connect();
+  await client.query(`DROP TABLE IF EXISTS "customer";`);
+  await client.end();
+}
---
+++ migrations/000003-product-category.ts
@@ -0,0 +1,40 @@
+import { DATABASE, DATABASE_HOST, DATABASE_PASSWORD, DATABASE_PORT, DATABASE_USER } from '@/env';
+import { Client } from 'pg';
+const client = new Client({
+  host: DATABASE_HOST,
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  port: DATABASE_PORT,
+});
+
+export async function up() {
+  await client.connect();
+  await client.query(`CREATE TABLE IF NOT EXISTS product_category
+  (
+      id smallserial NOT NULL,
+      name text NOT NULL,
+      PRIMARY KEY (id)
+  );`);
+
+  await client.query(`CREATE TABLE IF NOT EXISTS product_category
+  (
+      id smallserial NOT NULL,
+      name text NOT NULL,
+      PRIMARY KEY (id)
+  );`);
+
+  await client.query(`INSERT INTO product_category (name)
+  VALUES
+    ('lanche'),
+    ('acompanhamento'),
+    ('bebida'),
+    ('sobremesa');`);
+
+  await client.end();
+}
+
+export async function down() {
+  await client.connect();
+  await client.query(`DROP TABLE IF EXISTS "product_category";`);
+  await client.end();
+}
